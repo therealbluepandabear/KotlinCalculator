d059e8d2484c47a83046df183aed49193af94ee7
xTheCoderx <justaguyoney@gmail.com>
2021-10-11 15:43:10 +1300

    Added support for ActivityMainBinding..

--- app/src/main/java/com/example/kotlincalculator/MainActivity.kt
+++ app/src/main/java/com/example/kotlincalculator/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.kotlincalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kotlincalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setBindings()
+    }
+
+    private fun setBindings() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
