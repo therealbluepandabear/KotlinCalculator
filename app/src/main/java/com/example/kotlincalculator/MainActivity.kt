493d1ec954d7cef320e7f36c4927347ab150bac1
xTheCoderx <justaguyoney@gmail.com>
2021-10-11 19:59:44 +1300

    Created basic 'onClick()' method.

--- app/src/main/java/com/example/kotlincalculator/MainActivity.kt
+++ app/src/main/java/com/example/kotlincalculator/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.kotlincalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.kotlincalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +18,9 @@ class MainActivity : AppCompatActivity() {
     private fun setBindings() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    fun calculatorButtonClick(view: View) {
+        Toast.makeText(this, "Hi", Toast.LENGTH_LONG).show()
     }
 }
